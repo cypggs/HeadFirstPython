3c7bc99f40bbdc02ad898567e7e60928a6046222
qcypggs <qcypggs@qq.com>
2018-05-30 23:27:15 +0800

    a

---
+++ webapp/athletemodel.py
@@ -0,0 +1,34 @@
+import pickle
+import glob
+from athletelist import AthleteList
+def get_coach_data(filename):
+	try:
+		with open(filename) as f:
+			data = f.readline()
+		templ = data.strip().split(',')
+		return(AthleteList(templ.pop(0),templ.pop(0),templ))
+	except IOError as ioerr:
+		print('File error:' + str(ioerr))
+		return(None)
+def put_to_store(files_list):
+	all_athletes = {}
+	for each_file in files_list:
+		ath = get_coach_data(each_file)
+		all_athletes[ath.name] = ath
+	try:
+		with open('athletes.pickle','wb') as athf:
+			pickle.dump(all_athletes,athf)
+	except IOError as ioerr:
+		print ('File error(put_and_store):' + str(ioerr))
+	return(all_athletes)
+def get_from_store():
+	all_athletes = {}
+	try:
+		with open('athletes.pickle','rb') as athf:
+			all_athletes = pickle.load(athf)
+	except IOError as ioerr:
+		print('FIle error(get_from_store): + str(ioerr)')
+	return(all_athletes)
+data_files = glob.glob("data/*.txt")
+athletes = put_to_store(data_files)
+print athletes
