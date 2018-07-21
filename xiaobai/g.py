0ec30efa3b6af7a486a06a597dce2926b8191975
qcypggs <qcypggs@qq.com>
2018-07-21 15:27:01 +0800

    2

---
+++ xiaobai/g.py
@@ -0,0 +1,8 @@
+def gtokg(a):
+	return a*1000
+print gtokg(1)
+
+def zhijiao(b,c):
+	d = b*b+c*c;
+	return d
+print zhijiao(3,4)
---
+++ xiaobai/write.py
@@ -0,0 +1,2 @@
+file = open(r'a.txt\n','aw')
+file.write('leilei i love you')
